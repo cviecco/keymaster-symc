13fca6cc5848f9484969047ac84071daf2635ec2
Camilo Viecco <camilo_viecco1@symantec.com>
2017-07-18 10:46:04 -0700

    minor test file

---
+++ cmd/viptest/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/Symantec/keymaster/lib/vip"
+	"io/ioutil"
+	"os"
+)
+
+var (
+	certFilename = flag.String("cert", "", "The filename of the certificate")
+	certKey      = flag.String("key", "", "the key for the cert")
+	tokenID      = flag.String("tokenid", "", "The tokenID to test")
+	otpValue     = flag.Int("OTP", 1234, "The otp Value")
+	debug        = flag.Bool("debug", false, "Enable debug messages to console")
+)
+
+func exitsAndCanRead(fileName string, description string) ([]byte, error) {
+	if _, err := os.Stat(fileName); os.IsNotExist(err) {
+		return nil, err
+	}
+	buffer, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		err = errors.New("cannot read " + description + "file")
+		return nil, err
+	}
+	return buffer, err
+}
+
+func main() {
+	flag.Parse()
+	fmt.Println("vim-go")
+	certPem, err := exitsAndCanRead(*certFilename, "certificate file")
+	if err != nil {
+		panic(err)
+	}
+	keyPem, err := exitsAndCanRead(*certKey, "key file file")
+	if err != nil {
+		panic(err)
+	}
+
+	vipClient, err := vip.NewClient(certPem, keyPem)
+	if err != nil {
+		panic(err)
+	}
+	_, err = vipClient.VerifySingleToken(*tokenID, *otpValue)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("done")
+
+}
