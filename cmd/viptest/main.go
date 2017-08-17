97faa7604de9f861bc5632239019e671f9579b74
Camilo Viecco <camilo_viecco1@symantec.com>
2017-08-17 18:52:37 +0000

    adding new data

--- cmd/viptest/main.go
+++ cmd/viptest/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/xml"
 	"errors"
 	"flag"
 	"fmt"
@@ -9,14 +10,83 @@ import (
 	"os"
 )
 
+const ExampleUserInfoResponse = `<?xml version="1.0"?>
+<S:Envelope xmlns:S="http://schemas.xmlsoap.org/soap/envelope/">
+  <S:Body>
+    <GetUserInfoResponse xmlns="https://schemas.symantec.com/vip/2011/04/vipuserservices">
+      <requestId>258aaa0361ea</requestId>
+      <status>0000</status>
+      <statusMessage>Success</statusMessage>
+      <userId>username</userId>
+      <userCreationTime>2016-05-25T18:40:29.747Z</userCreationTime>
+      <userStatus>ACTIVE</userStatus>
+      <numBindings>1</numBindings>
+      <credentialBindingDetail>
+        <credentialId>AVTXXXXXXX</credentialId>
+        <credentialType>STANDARD_OTP</credentialType>
+        <credentialStatus>ENABLED</credentialStatus>
+        <bindingDetail>
+          <bindStatus>ENABLED</bindStatus>
+          <friendlyName>symentec-hw1</friendlyName>
+          <lastBindTime>2016-06-03T19:58:32.373Z</lastBindTime>
+          <lastAuthnTime>2017-08-03T21:58:22.090Z</lastAuthnTime>
+          <lastAuthnId>484374FC7EB7AA12</lastAuthnId>
+        </bindingDetail>
+      </credentialBindingDetail>
+    </GetUserInfoResponse>
+  </S:Body>
+</S:Envelope>`
+
 var (
-	certFilename = flag.String("cert", "", "The filename of the certificate")
-	certKey      = flag.String("key", "", "the key for the cert")
+	certFilename = flag.String("cert", "cert.pem", "The filename of the certificate")
+	certKey      = flag.String("key", "key.pem", "the key for the cert")
 	targetUrl    = flag.String("targetUrl", "https://vipservices-auth.verisign.com/val/soap", "the key for the cert")
 	tokenID      = flag.String("tokenid", "", "The tokenID to test")
 	otpValue     = flag.Int("OTP", 1234, "The otp Value")
 	debug        = flag.Bool("debug", false, "Enable debug messages to console")
 )
+
+type vipResponseBindingDetail struct {
+	ReasonCode    string `xml:"bindStatus,omitempty"`
+	FriendlyName  string `xml:"friendlyName,omitempty"`
+	LastBindTime  string `xml:"lastBindTime,omitempty"`
+	LastAuthnTime string `xml:"lastAuthnTime,omitempty"`
+	LastAuthnId   string `xml:"lastAuthnId,omitempty"`
+}
+type vipResponseCredentialBindingDetail struct {
+	CredentialId     string                   `xml:"credentialId,omitempty"`
+	CredentialType   string                   `xml:"credentialType,omitempty"`
+	CredentialStatus string                   `xml:"credentialStatus,omitempty"`
+	BindingDetail    vipResponseBindingDetail `xml:"bindingDetail"`
+}
+
+type vipResponseGetUserInfo struct {
+	RequestId               string                               `xml:"requestId"`
+	Status                  string                               `xml:"status"`
+	StatusMessage           string                               `xml:"statusMessage"`
+	UserId                  string                               `xml:"userId"`
+	UserCreationTime        string                               `xml:"userCreationTime"`
+	UserStatus              string                               `xml:"userStatus"`
+	NumBindings             string                               `xml:"numBindings"`
+	CredentialBindingDetail []vipResponseCredentialBindingDetail `xml:"credentialBindingDetail"`
+}
+
+type userInfoResponseBody struct {
+	XMLName xml.Name `xml:"Envelope"`
+	Body    struct {
+		VipResponseGetUserInfo vipResponseGetUserInfo `xml:"GetUserInfoResponse"`
+	}
+}
+
+func parseUserInfoResponse() {
+	var response userInfoResponseBody
+	err := xml.Unmarshal([]byte(ExampleUserInfoResponse), &response)
+	//err = xml.Unmarshal(responseBytes, &response)
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Printf("%+v", response)
+}
 
 func exitsAndCanRead(fileName string, description string) ([]byte, error) {
 	if _, err := os.Stat(fileName); os.IsNotExist(err) {
@@ -32,6 +102,8 @@ func exitsAndCanRead(fileName string, description string) ([]byte, error) {
 
 func main() {
 	flag.Parse()
+	parseUserInfoResponse()
+
 	fmt.Println("vim-go")
 	certPem, err := exitsAndCanRead(*certFilename, "certificate file")
 	if err != nil {
